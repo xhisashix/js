bfa85daf8eea6af342880eeb4009a461b291adba
Hisashi <hisasi622@gmail.com>
2020-11-03 00:30:47 +0900

    emit

---
+++ compornent/js/main.js
@@ -0,0 +1,39 @@
+(function () {
+  'use strict';
+
+  var likeComponent = Vue.extend({
+    props: {
+      message: {
+        default: 'like',
+        type: String,
+      },
+    },
+    data: function () {
+      return {
+        count: 0,
+      };
+    },
+    template: '<button @click="countUp">{{message}} {{ count }}</button>',
+    methods: {
+      countUp: function () {
+        this.count++;
+        this.$emit('increment');
+      },
+    },
+  });
+
+  var app = new Vue({
+    el: '#app',
+    components: {
+      'like-component': likeComponent,
+    },
+    data: {
+      total: 0,
+    },
+    methods: {
+      incrementTotal: function () {
+        this.total++;
+      },
+    },
+  });
+})();
